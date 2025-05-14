b4a6a5b1dd7d010473d5e0cc32fefe106012de85
i.vukolov <i.vukolov@Vrgaz.ru>
2025-05-14 13:39:34 +0300

    created MenuItem model

---
+++ menu/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.urls import reverse, NoReverseMatch, reverse_lazy
+from django.utils.translation import gettext_lazy
+
+class MenuItem(models.Model):
+    name = models.CharField(gettext_lazy('Название меню'), max_length=100)
+    menu_name = models.CharField(
+        'Название меню',
+        max_length=50,
+        help_text='Имя меню, к которому относится этот пункт'
+    )
+    named_url = models.CharField(
+        'Именованный URL',
+        max_length=100,
+        blank=True,
+        help_text='Именованный URL из urls.py'
+    )
+    url = models.CharField(
+        'Явный URL',
+        max_length=200,
+        blank=True,
+        help_text='Явный URL (если нет named URL)'
+    )
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children',
+        verbose_name = 'Родительский пункт меню'
+    )
+    order = models.PositiveIntegerField('Порядок стортировки', default=0)
+
+    class Meta:
+        verbose_name = 'Пункт меню'
+        verbose_name_plural = 'Пункты меню'
+        ordering = ['order']
+
+    def __str__(self):
+        return self.name
